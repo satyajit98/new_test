154c33293a799f2bf60e90761310ef654f4e41e0
satyajit98 <satyajitbhunia982000@gmail.com>
2021-11-08 11:54:40 +0530

    fifth commit

---
+++ factorial.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	int n,i,fact=1;
+	printf("Enter the number-");
+	scanf("%d",&n);
+	for(i=n;i>=1;i--)
+	{
+		fact=fact*i;
+	}
+	printf("The factrial number is-->%d\n",fact);
+	return 0;
+}
